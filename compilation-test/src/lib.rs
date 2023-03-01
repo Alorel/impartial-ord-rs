c37bdbe3a1f4ba4da2e4880cd57a9056104cc464
Art <amolc@pm.me>
2023-03-01 20:32:23 +0000

    feat: Initial impl

---
+++ compilation-test/src/lib.rs
@@ -0,0 +1,28 @@
+//! Mainly just test that these all compile
+
+#![allow(unused)]
+
+use impartial_ord::ImpartialOrd;
+use std::hash::Hash;
+
+#[derive(Eq, PartialEq, Ord, ImpartialOrd)]
+struct A;
+
+#[derive(Eq, PartialEq, Ord, ImpartialOrd)]
+struct B {}
+
+#[derive(Eq, PartialEq, Ord, ImpartialOrd)]
+struct C();
+
+#[derive(Eq, PartialEq, Ord, ImpartialOrd)]
+struct D(u8);
+
+#[derive(Eq, PartialEq, Ord, ImpartialOrd)]
+struct E {
+    x: u8,
+}
+
+#[derive(Eq, PartialEq, Ord, ImpartialOrd)]
+struct Foo<T: Clone, E>(T, E)
+where
+    E: Hash;
