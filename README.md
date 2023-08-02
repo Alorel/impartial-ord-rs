c9479623fb56e71669859936a5ef1e55036f86f6
Art <amolc@pm.me>
2023-08-02 22:56:28 +0100

    docs: Clean up documentation

--- README.md
+++ README.md
@@ -1,20 +1,28 @@
+<!-- cargo-rdme start -->
+
+Derives a quicker `PartialOrd` for types that already implement `Ord`.
+
 [![master CI badge](https://img.shields.io/github/actions/workflow/status/Alorel/impartial-ord-rs/ci.yml?label=master%20CI)](https://github.com/Alorel/impartial-ord-rs/actions/workflows/ci.yml?query=branch%3Amaster)
 [![crates.io badge](https://img.shields.io/crates/v/impartial-ord)](https://crates.io/crates/impartial-ord)
 [![docs.rs badge](https://img.shields.io/docsrs/impartial-ord?label=docs.rs)](https://docs.rs/impartial-ord)
 [![dependencies badge](https://img.shields.io/librariesio/release/cargo/impartial-ord)](https://libraries.io/cargo/impartial-ord)
 
-Derives a quicker `PartialOrd` for types that already implement `Ord`.
+```rust
+#[derive(impartial_ord::ImpartialOrd, Ord, PartialEq, Eq, Default, Debug)]
+struct MyStruct { foo: Bar, qux: Baz, }
+
+assert_eq!(MyStruct::default().partial_cmp(&MyStruct::default()), Some(Ordering::Equal));
+```
+
+### Generated output
 
 ```rust
-// Input
-#[derive(PartialEq, Eq, Ord, impartial_ord::ImpartialOrd)]
-struct MyStruct;
-
-// Output
 impl PartialOrd for MyStruct where Self: Ord {
     #[inline]
     fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
         Some(Ord::cmp(self, other))
     }
 }
-```
+````
+
+<!-- cargo-rdme end -->
--- src/lib.rs
+++ src/lib.rs
@@ -1,28 +1,39 @@
+//! Derives a quicker `PartialOrd` for types that already implement `Ord`.
+//!
+//! [![master CI badge](https://img.shields.io/github/actions/workflow/status/Alorel/impartial-ord-rs/ci.yml?label=master%20CI)](https://github.com/Alorel/impartial-ord-rs/actions/workflows/ci.yml?query=branch%3Amaster)
+//! [![crates.io badge](https://img.shields.io/crates/v/impartial-ord)](https://crates.io/crates/impartial-ord)
+//! [![docs.rs badge](https://img.shields.io/docsrs/impartial-ord?label=docs.rs)](https://docs.rs/impartial-ord)
+//! [![dependencies badge](https://img.shields.io/librariesio/release/cargo/impartial-ord)](https://libraries.io/cargo/impartial-ord)
+//!
 //! ```
 //! # use core::cmp::{PartialOrd, Ord, Ordering};
 //! # type Bar = u8;
 //! # type Baz = u8;
 //! #
-//! // Input
-//! #[derive(PartialEq, Eq, Ord, impartial_ord::ImpartialOrd)]
-//! # struct Impostor;
-//! # #[derive(PartialEq, Eq, Ord)]
+//! #[derive(impartial_ord::ImpartialOrd, Ord, PartialEq, Eq, Default, Debug)]
 //! struct MyStruct { foo: Bar, qux: Baz, }
 //!
-//! // Output
-//! impl PartialOrd for MyStruct where Self: Ord {
-//!   #[inline]
-//!   fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-//!     Some(Ord::cmp(self, other))
-//!   }
-//! }
+//! assert_eq!(MyStruct::default().partial_cmp(&MyStruct::default()), Some(Ordering::Equal));
 //! ```
+//!
+//! ### Generated output
+//!
+#![cfg_attr(doctest, doc = " ````no_test")]
+//! ```
+//! impl PartialOrd for MyStruct where Self: Ord {
+//!     #[inline]
+//!     fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+//!         Some(Ord::cmp(self, other))
+//!     }
+//! }
+//! ````
+//!
 
 use quote::quote;
-use syn::{parse2, parse_macro_input, DeriveInput, WherePredicate};
+use syn::{parse_macro_input, parse_quote, DeriveInput};
 
 /// A quicker [PartialOrd](core::cmp::PartialOrd) for types that already implement
-/// [Ord](core::cmp::Ord).
+/// [Ord](core::cmp::Ord). See [crate-level docs](crate).
 #[proc_macro_derive(ImpartialOrd)]
 pub fn derive_partial_ord(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     let DeriveInput {
@@ -32,11 +43,10 @@ pub fn derive_partial_ord(input: proc_macro::TokenStream) -> proc_macro::TokenSt
     } = parse_macro_input!(input as DeriveInput);
 
     let (gen_impl, gen_type, gen_where) = {
-        let self_predicate = match parse2::<WherePredicate>(quote! { Self: ::core::cmp::Ord }) {
-            Ok(p) => p,
-            Err(e) => return e.to_compile_error().into(),
-        };
-        generics.make_where_clause().predicates.push(self_predicate);
+        generics
+            .make_where_clause()
+            .predicates
+            .push(parse_quote! { Self: ::core::cmp::Ord });
         generics.split_for_impl()
     };
 
