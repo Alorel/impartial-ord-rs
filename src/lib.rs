e27ef4a0e3bbb6bf03fdbf15b46495cd554528ad
Art <4998038+Alorel@users.noreply.github.com>
2024-06-15 12:54:11 +0100

    ci: Reuse some actions from other repos

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 //! Derives a quicker `PartialOrd` for types that already implement `Ord`.
 //!
-//! [![master CI badge](https://img.shields.io/github/actions/workflow/status/Alorel/impartial-ord-rs/ci.yml?label=master%20CI)](https://github.com/Alorel/impartial-ord-rs/actions/workflows/ci.yml?query=branch%3Amaster)
+//! [![Master CI badge](https://github.com/Alorel/impartial-ord-rs/actions/workflows/lint-n-test.yml/badge.svg?branch=master)](https://github.com/Alorel/impartial-ord-rs/actions/workflows/lint-n-test.yml?query=branch%3Amaster)
 //! [![crates.io badge](https://img.shields.io/crates/v/impartial-ord)](https://crates.io/crates/impartial-ord)
 //! [![docs.rs badge](https://img.shields.io/docsrs/impartial-ord?label=docs.rs)](https://docs.rs/impartial-ord)
 //! [![dependencies badge](https://img.shields.io/librariesio/release/cargo/impartial-ord)](https://libraries.io/cargo/impartial-ord)
