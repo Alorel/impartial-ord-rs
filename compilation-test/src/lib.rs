56a3a2962200db79bc2b4d0968e8f821776bc371
Art <4998038+Alorel@users.noreply.github.com>
2024-06-15 12:42:09 +0100

    fix: Fully qualify path to Option

--- compilation-test/src/lib.rs
+++ compilation-test/src/lib.rs
@@ -9,38 +9,38 @@ use std::sync::atomic::AtomicU8;
 
 #[derive(Eq, PartialEq, Ord, ImpartialOrd)]
 struct A;
-assert_impl_any!(A: ::core::cmp::PartialOrd);
+assert_impl_any!(A: PartialOrd);
 
 #[derive(Eq, PartialEq, Ord, ImpartialOrd)]
 struct B {}
-assert_impl_any!(B: ::core::cmp::PartialOrd);
+assert_impl_any!(B: PartialOrd);
 
 #[derive(Eq, PartialEq, Ord, ImpartialOrd)]
 struct C();
-assert_impl_any!(C: ::core::cmp::PartialOrd);
+assert_impl_any!(C: PartialOrd);
 
 #[derive(Eq, PartialEq, Ord, ImpartialOrd)]
 struct D(u8);
-assert_impl_any!(D: ::core::cmp::PartialOrd);
+assert_impl_any!(D: PartialOrd);
 
 #[derive(Eq, PartialEq, Ord, ImpartialOrd)]
 struct E {
     x: u8,
 }
-assert_impl_any!(E: ::core::cmp::PartialOrd);
+assert_impl_any!(E: PartialOrd);
 
 #[derive(ImpartialOrd, Eq, PartialEq, Ord)]
 struct Foo<T: Clone, E>(T, E)
 where
     E: Hash;
-assert_impl_any!(Foo::<u8, u8>: ::core::cmp::PartialOrd);
+assert_impl_any!(Foo::<u8, u8>: PartialOrd);
 
 #[derive(Eq, PartialEq, Ord, ImpartialOrd)]
 struct G<T = u8>(T);
-assert_impl_any!(G::<u8>: ::core::cmp::PartialOrd);
-assert_not_impl_any!(G::<AtomicU8>: ::core::cmp::PartialOrd);
+assert_impl_any!(G::<u8>: PartialOrd);
+assert_not_impl_any!(G::<AtomicU8>: PartialOrd);
 
 #[derive(Eq, PartialEq, Ord, ImpartialOrd)]
 struct H<E, const T: usize = 0>(E, [u8; T]);
-assert_impl_any!(H::<u8>: ::core::cmp::PartialOrd);
-assert_not_impl_any!(H::<AtomicU8>: ::core::cmp::PartialOrd);
+assert_impl_any!(H::<u8>: PartialOrd);
+assert_not_impl_any!(H::<AtomicU8>: PartialOrd);
